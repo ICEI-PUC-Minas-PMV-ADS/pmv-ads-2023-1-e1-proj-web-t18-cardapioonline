5d3c5c957181e093018205c6224fe8a1643d28c9
Ieeeza <cesardermer1@gmail.com>
2023-06-07 20:53:01 -0300

    1.2

--- pagina/index.js
+++ pagina/index.js
@@ -24,7 +24,7 @@ for (var i = 0; i < addToCartButtons.length; i++) {
     addToCartButtons[i].addEventListener("click", addProductToCart )
 }
 
-function addProductToCart(event){
+async function addProductToCart(event){
     const button = event.target;
     const productInfos = button.parentElement;
     const productImage = productInfos.getElementsByClassName("product-image")[0].src;
@@ -42,18 +42,18 @@ function addProductToCart(event){
     let newCartProduct = document.createElement("tr");
     newCartProduct.classList.add("cart-product");
 
-    newCartProduct.innerHTML = 
+    newCartProduct.innerHTML =
     `
     <td class="product-identification">
         <img src=${productImage} alt="${productTitle}" width="40px" height="40px">
         <strong class="text-warning me-1 cart-product-title">${productTitle}</strong>
     </td>
     <td>
-        <span class="text-warning">${productPrice}</span>
+        <span class="text-warning cart-product-price">${productPrice}</span>
     </td>
     <td>
-        <input type="number" value="1" min="0" class="input-group mt-2 rounded small product-qtd-input">
-        <button type="button" class="btn btn-primary">Remover</button> 
+        <input type="number" value="1" min="0" class="rounded small product-qtd-input">
+        <button type="button" class="remove-product-button btn btn-primary">Remover</button> 
     </td>
     `
 
@@ -61,4 +61,18 @@ function addProductToCart(event){
     tableBody.append(newCartProduct);
 }
  
- 
+const removeProductButtons = document.getElementsByClassName("remove-product-button");
+for (var i = 0; i < removeProductButtons.length; i++) {
+    removeProductButtons[i].addEventListener("click", function(event){
+        event.target.parentElement.parentElement.remove();
+    })
+}
+
+let totalAmount = 0;
+const cartProducts = document.getElementsByClassName("cart-product")
+for(var i = 0; i < cartProducts.length; i++) {
+    const productPrice = cartProducts[i].getElementsByClassName("cart-product-price")[0].innerText.replace("R$", "").replace(",", ".");
+    const productQuantity = cartProducts[i].getElementsByClassName("product-qtd-input")[0].value;
+    console.log(productPrice)
+    totalAmount = totalAmount + (productPrice * productQuantity);
+}
